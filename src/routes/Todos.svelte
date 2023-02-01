2390e23000bddec661ff2f411c0e98370045cf10
notusknot <notusknot@gmail.com>
2023-02-01 07:53:13 -0800

    Improve UI

--- src/routes/Todos.svelte
+++ src/routes/Todos.svelte
@@ -60,8 +60,11 @@ async function toggleChecked(todo: Todo) {
 <div>
   {#each $todos as todo (todo.id)}
 		<div class="todo">
-		    <input type="checkbox" id="checkbox" checked={todo.checked} on:change={() => toggleChecked(todo)} />
-		    <span class:checked={todo.checked}>{todo.text}</span>
+        <label class="checkbox">
+  		    <input type="checkbox" checked={todo.checked} on:change={() => toggleChecked(todo)} />
+          <span class="checkmark"></span>
+        </label>
+		    <span class="todo-text" class:checked={todo.checked}>{todo.text}</span>
 	    <button on:click={() => deleteTodo(todo)}>x</button>
 		</div>
   {/each}
@@ -71,11 +74,65 @@ async function toggleChecked(todo: Todo) {
   <input placeholder="add something" type="text" bind:value={todoText} on:blur={addTodo} />
 </form>
 
-
-
 <style>
 .todo {
 	display: flex;
+  justify-content: space-between;
+  padding: calc(var(--padding) / 4) 0;
+}
+
+.todo-text {
+  padding: 0 var(--padding);
+  width: 100%;
+}
+
+button {
+  padding: 0px;
+  min-width: 24px;
+  height: 24px;
+  border: none;
+  background-color: var(--bg);
+}
+
+.checkbox {
+  display: inline-block;
+  position: relative;
+  min-width: 24px;
+  height: 24px;
+  border: 3px solid var(--text);
+  border-radius: 8px;
+  transition: all 0.2s;
+  cursor: pointer;
+}
+
+.checkbox:hover, .checkbox:active {
+  border: 3px solid var(--accent);
+}
+
+.checkbox input[type="checkbox"] {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.checkbox .checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+.checkbox input[type="checkbox"]:checked ~ .checkmark:after {
+  display: block;
+}
+
+.checkbox .checkmark:after {
+  left: 7px;
+  top: 3px;
+  width: 5px;
+  height: 10px;
+  border: solid var(--text);
+  border-width: 0 3px 3px 0;
+  transform: rotate(45deg);
 }
 
 .checked {
