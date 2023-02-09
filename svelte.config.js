7b722ef1585263888a7514fb9391e8a724beb2f2
notusknot <notusknot@gmail.com>
2023-02-09 12:01:21 -0800

    Hopefully fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			edge: true
+			runtime: "edge"
 		}),
 	},
 };
